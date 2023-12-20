eeaa0cf576c72229c2cf7c98cbba143e50ae362b
rgoncalvss <carolinor7@gmail.com>
2023-12-20 15:49:48 -0300

    refact/add env variables on database config

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log('Server started at port 3000'));
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server started at port ${port}`));
