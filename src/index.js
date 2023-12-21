6a2bd4d3b9a3a6cdd18c67366cb19fe7a8af14cb
rgoncalvss <carolinor7@gmail.com>
2023-12-20 23:17:28 -0300

    feature/async-middleware-error-handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,6 +7,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started at port ${port}`));
